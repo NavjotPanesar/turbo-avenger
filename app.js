bf0799121689340de233661731f900df6095bd71
Navjot Panesar <navjotspanesar@gmail.com>
2015-01-31 13:18:44 -0500

    Added google auth

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//Mongo
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/todolist');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+  
+var index = require('./routes/');
 
 var app = express();
 
@@ -16,14 +20,14 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(session({secret: 'ecemasterrace'}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +59,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
---
+++ routes/auth/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = module.exports = express();
+
+var mongoose = require('mongoose');
+var User = require('../user.model.js');
+
+ var passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+app.use(passport.initialize());
+app.use(passport.session()); 
+	
+app.get('/', function(req, res) {
+    res.send('auth');
+});
+
+var GOOGLE_CLIENT_ID = "112960821724-h76qs8lp9veg1bnhoesv082vdbj3tahq.apps.googleusercontent.com";
+var GOOGLE_CLIENT_SECRET = "ppl0nLHkIECsIMO_rpJKR7ET";
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/callback",
+        passReqToCallback: true
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+        var user = new User({
+            googleId: profile.id
+        });
+        user.save(function(err, user) {
+			request.session.user = user;
+			return done(err, user);
+        });
+    }
+));
+
+
+app.get('/google',
+    passport.authenticate('google', {
+        scope: ['https://www.googleapis.com/auth/plus.login', , 'https://www.googleapis.com/auth/plus.profile.emails.read']
+    }));
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/auth/google/failure'
+    }));

f4d0112f6260e877471b84428508ac10de69ce77
lloydtorres <lloyd.torres95@gmail.com>
2015-02-01 08:48:39 -0500

    Add info panel + other UI fixes

--- public/controllers/questController.js
+++ public/controllers/questController.js
@@ -22,7 +22,7 @@ app.controller('questController', ['$scope', '$http', function($scope, $http){
     $scope.init();
 
     $scope.subscribeList = function(){
-        var stuff = prompt("Enter the ID of the list to subscribe:");
+        var stuff = prompt("Enter the ID of the challenge to add:");
         if (stuff != null) {
             $http.post('/masterlists/subscribe',  { 'id' : stuff });
             location.reload();
